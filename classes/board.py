6d628fa79917ef0b51a85a5b7de8e3da8d08e5d2
Beto <100471939@alumnos.uc3m.es>
2022-03-20 13:56:35 +0100

    game

---
+++ classes/board.py
@@ -0,0 +1,8 @@
+import pyxel 
+
+class board:
+    def __init__(self) -> None:
+        ...
+    
+    def draw():
+        pyxel.cls()
--- game.py
+++ game.py
@@ -1,5 +1,18 @@
 import pyxel
+import settings
+from classes import *
 
 class game:
     def __init__(self) -> None:
-        pyxel.init()
+        pyxel.init(settings.ancho_pantalla,settings.alto_pantalla,caption = "Super orto mega ajedrez", fps = settings.fps)
+
+        pyxel.mouse(True)
+        pyxel.run(self.update,self.draw)
+
+    def update(self):
+        pass
+    
+    def draw(self):
+        pyxel.cls(1)
+
+game()
