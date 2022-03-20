0c5a5c9d1804d2b55a65c01f0ebe0cfb4fe4d2d8
Ragarr <100472050@alumnos.uc3m.es>
2022-03-20 14:03:48 +0100

    assets

--- game.py
+++ game.py
@@ -1,10 +1,10 @@
 import pyxel
-import settings
+import settings as st
 from classes import *
 
 class game:
     def __init__(self) -> None:
-        pyxel.init(settings.ancho_pantalla,settings.alto_pantalla,caption = "Super orto mega ajedrez", fps = settings.fps)
+        pyxel.init(st.ancho_pantalla,st.alto_pantalla,caption = "Super orto mega ajedrez", fps = st.fps)
 
         pyxel.mouse(True)
         pyxel.run(self.update,self.draw)
