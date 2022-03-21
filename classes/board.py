bc80a79e1bf38d4d9d5fc7fff7088f3f52f39866
Ragarr <100472050@alumnos.uc3m.es>
2022-03-21 10:24:26 +0100

    fusionado board con game por comodidad

--- classes/board.py
+++ classes/board.py
@@ -3,7 +3,7 @@ import numpy as np
 if __name__ == "__main__":
     print("this file is not suposed to be run")
     quit()
-
+'''
 class Board:
     def __init__(self) -> None:
         self._sprite=[1,0,0,191,191,st.black]
@@ -15,4 +15,4 @@ class Board:
     def update(self,pieces:list):
         for piece in pieces:
                     self.array[abs(8-piece.position[1]),(piece.position[0]-1)]=piece
-            
+            '''
--- game.py
+++ game.py
@@ -1,5 +1,5 @@
 import pyxel
-from classes.Board import Board
+# from classes.Board import Board
 from classes.pieces.Rook import Rook
 from classes.pieces.Pawn import Pawn
 from classes.pieces.Queen import Queen
@@ -7,24 +7,26 @@ from classes.pieces.King import King
 from classes.pieces.Knight import Knight
 from classes.pieces.Bishop import Bishop
 import settings as st
-
+import numpy as np
 
 class game:
     def __init__(self) -> None:
         pyxel.init(st.screen_width,st.screen_height,caption = "Super orto mega ajedrez", fps = st.fps,scale=4)
         pyxel.load(st.assets_path)
         pyxel.mouse(True)
-        self.__board = Board()
-        self.pieces=[]
+        self.__board_sprite=[0,0,1,0,0,191,191,st.black]
+        self.array=np.full([8, 8], '▨')
         self.pieces=[]
         self.init_pieces()
         # esta linea siempre al final 
         pyxel.run(self.update,self.draw)
+    def __str__(self) -> str:
+        return str(np.array2string(self.array, formatter={'all': lambda x: str(x)}))+'\n'
 
     def update(self):
-        print(self.__board)
-        self.__board.update(self.pieces)
-    
+        print(self)
+        self.update_board()
+        
     def draw(self):
         pyxel.cls(st.light_brown)
         self.draw_board()
@@ -38,9 +40,12 @@ class game:
             pyxel.blt(*piece.coord,*piece._sprite)
     def draw_board(self):
         '''just draws the board'''
-        pyxel.blt(0,0,*self.__board._sprite)
+        pyxel.blt(*self.__board_sprite)
     
-    
+    def update_board(self):
+        for piece in self.pieces:
+            self.array[abs(8-piece.position[1]),(piece.position[0]-1)]=piece
+
     def init_pieces(self):
         '''sets every piece on his initial coordinate'''
         for i in range(1,9,1):
