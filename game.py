ade0de767d8d8daa1b5e07c3519a99e144275144
Ragarr <100472050@alumnos.uc3m.es>
2022-03-22 10:52:55 +0100

    usando la libreria de chess a ver que tal

--- game.py
+++ game.py
@@ -1,11 +1,7 @@
 import pyxel
-from classes.Board import Board
-from classes.pieces.Rook import Rook
-from classes.pieces.Pawn import Pawn
-from classes.pieces.Queen import Queen
-from classes.pieces.King import King
-from classes.pieces.Knight import Knight
-from classes.pieces.Bishop import Bishop
+import sys
+import timeit
+import chess_game
 import settings as st
 
 
@@ -14,10 +10,7 @@ class game:
         pyxel.init(st.screen_width,st.screen_height,caption = "Super orto mega ajedrez", fps = st.fps,scale=4)
         pyxel.load(st.assets_path)
         pyxel.mouse(True)
-        self.__board = Board()
-        self.pieces=[]
-        self.pieces=[]
-        self.init_pieces()
+        board=
         # esta linea siempre al final 
         pyxel.run(self.update,self.draw)
 
@@ -31,38 +24,6 @@ class game:
         self.draw_pieces()
     
 
-    def draw_pieces(self):
-        for piece in self.pieces:
-            pyxel.blt(*piece.coord,*piece._sprite)
-        for piece in self.pieces:
-            pyxel.blt(*piece.coord,*piece._sprite)
-    def draw_board(self):
-        '''just draws the board'''
-        pyxel.blt(0,0,*self.__board._sprite)
     
-    
-    def init_pieces(self):
-        '''sets every piece on his initial coordinate'''
-        for i in range(1,9,1):
-            self.pieces.append(Pawn([i,7]))
-        self.pieces.append(Rook(['a',8]))
-        self.pieces.append(Rook(['h',8]))
-        self.pieces.append(Queen(['e',8]))
-        self.pieces.append(King(['d',8]))
-        self.pieces.append(Bishop(['f',8]))
-        self.pieces.append(Bishop(['c',8]))
-        self.pieces.append(Knight(['b',8]))
-        self.pieces.append(Knight(['g',8]))
-
-        for j in range(1,9,1):
-            self.pieces.append(Pawn([j,2],False))
-        self.pieces.append(Rook(['a',1],False))
-        self.pieces.append(Rook(['h',1],False))
-        self.pieces.append(Queen(['e',1],False))
-        self.pieces.append(King(['d',1],False))
-        self.pieces.append(Bishop(['f',1],False))
-        self.pieces.append(Bishop(['c',1],False))
-        self.pieces.append(Knight(['b',1],False))
-        self.pieces.append(Knight(['g',1],False))
     
 game()
